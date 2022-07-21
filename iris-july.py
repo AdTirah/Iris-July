9eb3765ffc66727c290e63080d880ebd84d04f72
AdTirah <109711233+AdTirah@users.noreply.github.com>
2022-07-21 17:04:17 +0800

    Update iris-july.py

--- iris-july.py
+++ iris-july.py
@@ -7,6 +7,8 @@ st.write("""
 This app predicts the **Iris flower** type!
 """)
 
+st.image(https://s3.amazonaws.com/assets.datacamp.com/blog_assets/Machine+Learning+R/iris-machinelearning.png)
+
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
